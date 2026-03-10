6d2af1508f05e2b02f7baf774e0298e86d953fd2
Nnta <nathan.bouvier@ens-lyon.fr>
2026-03-10 18:47:57 +0100

    Remove ignored files from repository

--- test.py
+++
@@ -1,6 +0,0 @@
-import utils, kernels
-
-dataset_train = utils.KernelDataset(x_csv_path="./Xtr.csv", y_csv_path="./Ytr.csv")
-dataset_test = utils.KernelDataset(x_csv_path="./Xte.csv")
-
-dataloader_train = utils.KernelDataLoader(dataset_test)
